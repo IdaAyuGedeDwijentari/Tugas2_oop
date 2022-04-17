0a1e6d54b7e3e82371affafd0f7ea17b81f2139f
IdaAyuGedeDwijentari <101305025+IdaAyuGedeDwijentari@users.noreply.github.com>
2022-04-17 21:33:18 +0800

    Add files via upload

---
+++ Volumebalok.java
@@ -0,0 +1,46 @@
+package t2;
+/**
+ *
+ * @idaayugededwijentari 20103015
+ */
+public class Volumebalok {
+    double volume;
+    double panjang;
+    double lebar;
+    double tinggi;
+    
+    public Volumebalok(double Pj, double Lb, double Tg){
+        this.setPj (Pj);
+        this.setLb(Lb);
+        this.setTg(Tg);
+        
+        this.panjang = Pj;
+        this.lebar = Lb;
+        this.tinggi = Tg;
+    }
+       public void setPj(double Pj) {
+        this.panjang = Pj;
+       }
+       public void setTg (double Tg){
+        this.tinggi = Tg;
+       }
+       public void setLb (double Lb) {
+        this.lebar = Lb;
+       }
+       double Hitungvolume (){
+           this.volume = this.panjang*this.lebar*this.tinggi;
+           return this.volume;
+       }
+       public void cetakvolume (){
+        this.Hitungvolume();
+        System.out.println("******MENGHITUNG VOLUME BALOK******");
+        System.out.println("NILAI Panjang   :"+this.panjang+"cm");
+        System.out.println("NILAI Lebar     :"+this.lebar+"cm");
+        System.out.println("NILAI Tinggi    :"+this.tinggi+"cm");
+        System.out.println("Volume Balok    :"+this.volume+"cm");
+       }
+       
+    }
+    
+    
+
