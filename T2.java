aa6fcdcb4a4c1307623ed30501af9870da599a22
IdaAyuGedeDwijentari <101305025+IdaAyuGedeDwijentari@users.noreply.github.com>
2022-04-17 20:35:32 +0800

    Add files via upload

---
+++ T2.java
@@ -0,0 +1,13 @@
+package t2;
+
+/**
+ *
+ * @idaayugededwijentari 20103015
+ */
+public class T2 {
+    public static void main(String[] args) {
+        Volumebalok balok1 = new Volumebalok(5,6,7);
+        balok1.cetakvolume ();
+    }
+    
+}
